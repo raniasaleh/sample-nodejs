821cb66ff85cf4c3c74051f5ae12caaa906c6fdf
raniasaleh <31638530+raniasaleh@users.noreply.github.com>
2023-01-28 13:16:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Handling GET request
 app.get('/', (req, res) => {
-	res.send('Hi there, A simple Node App is '
+	res.send('Hi there 2, A simple Node App is '
 		+ 'running on this server')
 	res.end()
 })
